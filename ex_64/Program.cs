852b4882eb20c34ab7e79a694a9a2168cadf7fa5
BelovEA <vrachea676@gmail.com>
2023-02-24 17:34:53 +0600

    hw is done

---
+++ ex_64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке 
+//от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("EX_64");
+Console.Write("Enter number > 0 to see all numbers between number and 0: ");
+int input;
+if (!int.TryParse(Console.ReadLine(), out input) || input <= 0)
+{
+    Console.WriteLine("Invalid input. Please enter a positive integer.");
+}
+else
+{
+    Console.WriteLine(NumbersToN(input));
+}
+
+string NumbersToN(int n)
+{
+    if (n == 0 || n < 0)
+    {
+        return String.Empty;
+    }
+    else
+    {
+        return $"{n} " + NumbersToN(n - 1);
+    }
+}
---
+++ ex_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.WriteLine("ex_66");
+
+int SumOfElMToN(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    else
+    {
+        return m + SumOfElMToN(m + 1, n);
+    }
+}
+
+Console.WriteLine("Enter two integers m and n, with m < n:");
+int m, n;
+if (!int.TryParse(Console.ReadLine(), out m) || !int.TryParse(Console.ReadLine(), out n) || m >= n)
+{
+    Console.WriteLine("Invalid input. Please enter two valid integers with m < n.");
+}
+else
+{
+    Console.WriteLine("Sum of numbers between m and n is: " + SumOfElMToN(m, n));
+}
